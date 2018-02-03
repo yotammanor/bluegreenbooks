344e4c800e6e1fddc0eff5e707ae750922a923d6
Yotam Manor <yotammanor@gmail.com>
2018-02-03 18:35:38 +0200

    add initial app setup.

---
+++ green/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=200)
+    has_leather_binding = models.BooleanField(default=False)
+
+    def __str__(self):
+        return (f"{self.name}"
+                f"{', in leather' if self.has_leather_binding else ''}")
---
+++ green/views.py
@@ -0,0 +1,7 @@
+from green.models import Book
+from django.http.response import JsonResponse
+
+
+def books(request):
+    data = [{"name": x.__str__()} for x in Book.objects.all()]
+    return JsonResponse(data=data, safe=False)
