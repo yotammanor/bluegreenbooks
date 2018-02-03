7af673e174e473a91cc936c81bc3ff9151afcb8c
Yotam Manor <yotammanor@gmail.com>
2018-02-03 20:36:14 +0200

    rename AppConfig class

--- bluegreenbooks/settings.py
+++ bluegreenbooks/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'green.apps.CoreConfig',
+    'green.apps.GreenConfig',
     'router.apps.RouterConfig',
 ]
 
--- green/apps.py
+++ green/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class CoreConfig(AppConfig):
+class GreenConfig(AppConfig):
     name = 'green'
