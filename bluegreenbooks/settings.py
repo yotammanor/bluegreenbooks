0425ae13b7f3fdd7d525408ab59128f261f94bd1
Yotam Manor <yotammanor@gmail.com>
2018-02-03 21:11:30 +0200

    add blue app

--- bluegreenbooks/settings.py
+++ bluegreenbooks/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'green.apps.GreenConfig',
+    'blue.apps.BlueConfig',
     'router.apps.RouterConfig',
 ]
 
