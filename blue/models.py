8b610a6da9228cb193f2c8d15ad0e961237b4b61
Yotam Manor <yotammanor@gmail.com>
2018-02-03 22:07:29 +0200

    change the blue app

--- blue/models.py
+++ blue/models.py
@@ -3,12 +3,9 @@ from django.db import models
 
 class Book(models.Model):
     name = models.CharField(max_length=200)
-    has_leather_binding = models.BooleanField(default=False)
 
     class Meta:
         db_table = "green_book"
-        managed = False
 
     def __str__(self):
-        return (f"{self.name}"
-                f"{', in leather' if self.has_leather_binding else ''}")
+        return f"{self.name}"
--- bluegreenbooks/settings.py
+++ bluegreenbooks/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'ocb_=1c^4xvs#&$ca*vc@=(^h=17kks5yg%*o!cwq77(kly8hh'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -55,6 +53,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'bluegreenbooks.urls'
+MIGRATION_NAME_SWITCH = '0002_remove_unwanted_field'
 
 TEMPLATES = [
     {
@@ -75,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bluegreenbooks.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
@@ -85,7 +83,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -105,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
@@ -118,7 +114,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
