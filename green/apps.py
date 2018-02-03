850aab6177735cf95622b440355691a189bc0676
Yotam Manor <yotammanor@gmail.com>
2018-02-03 22:19:25 +0200

    remove green app

--- green/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GreenConfig(AppConfig):
-    name = 'green'
--- green/views.py
+++
@@ -1,7 +0,0 @@
-from .models import Book
-from django.http.response import JsonResponse
-
-
-def books(request):
-    data = [{"name": x.__str__()} for x in Book.objects.all()]
-    return JsonResponse(data=data, safe=False)
